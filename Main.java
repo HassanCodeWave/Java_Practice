ef3782d3812fa20493ee9f92c21508de7c069c41
HassanCodeWave <shforshaikhhassan@gmail.com>
2024-04-27 00:10:27 +0500

    sample-commit

---
+++ Main.java
@@ -0,0 +1,93 @@
+//import javax.sound.midi.Soundbank;
+//import java.util.*;
+//
+//public class Main {
+//    public static void main(String[] args) {
+//
+////       Scanner sc =  new Scanner(System.in);
+////        System.out.println("Enter your age ");
+//        // System.out.println("Enter name, age and salary:")
+//
+//        // String name = sc.nextLine();
+////        int age = sc.nextInt();
+//        // double salary = sc.nextDouble();
+//
+////        if(age % 2 ==0){
+////            System.out.println("Your age is in EVEN Number");
+////        } else {
+////            System.out.println("Your age is in ODD Number");
+////        }
+//
+//        // System.out.println("Name: " + name)
+//        // System.out.println("Age: " + age)
+//        // System.out.println("Salary: " + salary);
+//
+////        switch (age){
+////            case 18:
+////                System.out.println("ADULT");
+////                break;
+////            case 19:
+////                System.out.println("NOT ADULT");
+////                break;
+////            default:
+////                System.out.println("DEFAULT");
+////
+////        }
+//
+////        Scanner sc = new Scanner(System.in);
+////        System.out.println("Enter age");
+////        int age= sc.nextInt();
+////        System.out.println("Your age is: " + age);
+//
+////      if(age > 18){
+////          System.out.println("Greater");
+////      } else if(age == 18){
+////          System.out.println("Equal");
+////      }
+////      else if(age<18){
+////          System.out.println("Less");
+////      }
+////      else {
+////            System.out.println("Your age is default");
+////        };
+//
+//      // FOR LOOP
+////      Scanner sc = new Scanner(System.in);
+////        System.out.println("Enter Number");
+////        int n = sc.nextInt();
+////        System.out.println("Multiplication table for " + n + ":");
+////        for (int i = 1; i <= 10; i++) {
+////            System.out.println(n + " x " + i + " = " + (n * i));
+////        }
+//// EVEN NUMBERS
+////        for (int i = 2; i <= n; i += 2) {
+////            System.out.println(i);
+////        }
+//
+//        //Make a menu driven program. The user can enter 2 numbers, either 1 or 0.
+////If the user enters 1 then keep taking input from the user for a student’s marks(out of 100).
+////If they enter 0 then stop.
+////If he/ she scores :
+////Marks >=90 -> print “This is Good”
+////        89 >= Marks >= 60 -> print “This is also Good”
+////        59 >= Marks >= 0 -> print “This is Good as well”
+////        Scanner sc = new Scanner(System.in);
+////        System.out.println("Enter Number 0 or 1");
+////        int n = sc.nextInt();
+//        int n=4;
+//        int m=5;
+//        for(int i=1;i<=n;i++){
+//            for(int j=1;i<=m;j++){
+//                System.out.print(" * ");
+//            }
+//            System.out.println();
+//        }
+//    }
+//}
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("j");
+
+    }
+}
+
